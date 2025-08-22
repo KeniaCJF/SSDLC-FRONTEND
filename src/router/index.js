867802a45e3873c628d01658af87b1a1031277cf
KeniaCJF~ <keniacarolina.j@gmail.com>
2025-08-21 18:17:05 -0600

    cambio

--- src/router/index.js
+++ src/router/index.js
@@ -7,14 +7,28 @@ import CreateUser from '@/components/CreateUser.vue'
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/incidences', component: Incidences },
-  { path: '/generate_incidence', component: GenerateIncidence },
-  { path: '/create-user', component: CreateUser },
+  { path: '/incidences', component: Incidences, meta: { requiresAuth: true } },
+  { path: '/generate-incidence', component: GenerateIncidence, meta: { requiresAuth: true } },
+  { path: '/create-user', component: CreateUser, meta: { requiresAuth: true } },
+
+  { path: '/:pathMatch(.*)*', redirect: '/login' }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+
+import { useUserStore } from '@/stores/user'
+
+router.beforeEach((to, from, next) => {
+  const userStore = useUserStore()
+  if (to.meta.requiresAuth && !userStore.token) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
